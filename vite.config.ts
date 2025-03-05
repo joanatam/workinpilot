ae92b5546314f9448b187999323772fd72571aec
joanatam <jcarroll@agendamate.org>
2025-03-05 15:49:44 -0500

    Restore orig vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
-const PORT = 80;
+const PORT = 3039;
 
 export default defineConfig({
   plugins: [
